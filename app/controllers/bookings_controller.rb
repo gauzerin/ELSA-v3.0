3366c7c520806530b424e8fbe7b581f92aa7f13e
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-23 18:36:51 +0100

    booking controller added, basic paths, booking policy added, needs testing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,57 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = policy_scope(Booking).where(user: current_user)
+  end
+
   def new
+    @Bed = Bed.find(params[:bed_id])
+    @booking = Booking.new
   end
 
   def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def index
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @bed = Bed.find(params[:bed_id])
+    @bed.hostel = @bed
+    authorize @booking
+    if @booking.save
+      redirect_to checkout_path, notice: "Booking successfully created"
+    else
+      render :new
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id]) # this is a show booking details page
+    authorize @booking
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    @booking.user = current_user
+    @booking.save
+  end
+
+  def update
+    authorize @booking
+    if @booking.update(booking_params)
+      redirect_to bookings_path, notice: "Booking successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @booking
+    @booking = Booking.find(params[:id])
+    if @booking.destroy
+      redirect_to bookings_path, notice: 'Hostel was succesfully removed'
+    else
+      render :index
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_at, :end_at)
   end
 end
