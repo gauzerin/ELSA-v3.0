898031a3fc1d120b353cec96ca4990645e80be1c
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-24 12:47:34 +0100

    added meta tags, added bootstrap, added alerts, jquery, bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_130148) do
+ActiveRecord::Schema.define(version: 2019_05_24_111021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_23_130148) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.date "start_ad"
+    t.date "start_at"
     t.date "end_at"
     t.float "total_cost"
     t.bigint "user_id"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 2019_05_23_130148) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo1"
+    t.string "photo2"
+    t.string "photo3"
     t.index ["user_id"], name: "index_hostels_on_user_id"
   end
 
@@ -56,6 +59,9 @@ ActiveRecord::Schema.define(version: 2019_05_23_130148) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_type"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
