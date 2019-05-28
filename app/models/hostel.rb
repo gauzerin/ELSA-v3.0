b7bfa3ac5af75119ff385835ff1c2e1b426d0dd4
Ottavia Perotti <perottiottavia@gmail.com>
2019-05-28 14:13:06 +0100

    Dependent destroy method on beds to hostels

--- app/models/hostel.rb
+++ app/models/hostel.rb
@@ -1,6 +1,6 @@
 class Hostel < ApplicationRecord
   belongs_to :user
-  has_many :beds
+  has_many :beds, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
