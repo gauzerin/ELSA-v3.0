d8ccb29c3d6f322dc648352b4e3e7fc7080d6ff5
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-29 15:19:05 +0100

    Logic for bookings beds and validating their availability completed, must add model validation and date selection validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,23 +3,23 @@ class Booking < ApplicationRecord
 
   validates :start_at, :end_at, presence: :true
 
-  def beds
-    @beds = Bed.where(slug: attributes[:beds].map { |bed_id| bed_id.to_i })
-  end
+  # def beds
+  #   Bed.where(slug: attributes[:beds].map { |bed_id| bed_id.to_i })
+  # end
 
-  def unavailable_dates
-    bookings.pluck(:start_at, :end_at).map do |range|
-      { from: range[0], to: range[1] }
-    end
-  end
+#   def unavailable_dates
+#     bookings.pluck(:start_at, :end_at).map do |range|
+#       { from: range[0], to: range[1] }
+#     end
+#   end
 
-private
+# private
 
-  def end_must_be_after_start
-    return if end_at.blank? || start_at.blank?
+#   def end_must_be_after_start
+#     return if end_at.blank? || start_at.blank?
 
-    if end_at < start_at
-      errors.add(:end_at, "Your stay must end after the start date")
-    end
-  end
+#     if end_at < start_at
+#       errors.add(:end_at, "Your stay must end after the start date")
+#     end
+#   end
 end
