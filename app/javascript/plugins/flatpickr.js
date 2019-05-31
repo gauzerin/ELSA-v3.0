f896567298b9ced67a42003b37f98605ae2332c8
Piotr Antonowicz <pantonowicz@gmail.com>
2019-05-31 17:05:50 +0100

    minor tweaks

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,11 +3,8 @@ import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr(".datepicker", {
-  mode: "range",
   minDate: "today"
-})
-
-flatpickr("#booking_start_at", {
+}, "#booking_start_at", {
   altInput: true,
   plugins: [new rangePlugin({ input: "#booking_end_at"})]
 })
