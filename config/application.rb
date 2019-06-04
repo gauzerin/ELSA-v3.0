7ba1b7ff7d0c2be1394a7047c6733516549ce0f2
Piotr Antonowicz <pantonowicz@gmail.com>
2019-06-04 17:29:02 +0100

    added hompage video

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ELSAV30
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << '#{Rails.root}/app/assets/videos'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
